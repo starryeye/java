ec57ece33e31cd1e4609b0df6907ee71b8172fd6
starryeye <see2818@naver.com>
2023-08-01 00:12:22 +0900

    [starryeye, 2023.08.01] - Java 함수형 인터페이스
    내용
    - Java 함수형 인터페이스

--- java8/src/main/java/dev/practice/java8/one/Foo.java
+++ java8/src/main/java/dev/practice/java8/one/Foo.java
@@ -23,7 +23,7 @@ public class Foo {
          * 함수형 인터페이스를 in-line 으로 구현한 오브젝트이다.
          *
          * 자바에서는 함수가 특수한 형태의 오브젝트 이므로, 함수가 함수를 파라미터로 받고 함수가 함수를 리턴할 수 있다.
-         * -> 고차 함수 (High-Order Class)
+         * -> 고차 함수 (Higher-Order Class)
          */
         RunSomething runSomething2 = () -> System.out.println("doIt");
         runSomething2.doIt();
