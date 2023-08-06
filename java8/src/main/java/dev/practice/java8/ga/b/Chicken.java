9f06fc819711dfe6f8af55d7d2f2f0521d1d9cd9
starryeye <see2818@naver.com>
2023-08-06 20:38:25 +0900

    [starryeye, 2023.08.06] - Java Annotation
    내용
    - Java Annotation

---
+++ java8/src/main/java/dev/practice/java8/ga/b/Chicken.java
@@ -0,0 +1,11 @@
+package dev.practice.java8.ga.b;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_USE)
+@Repeatable(ChickenContainer.class) // 반복할 수 있는 어노테이션으로 만듬
+public @interface Chicken {
+
+    String value();
+}
