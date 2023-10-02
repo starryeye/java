a9df6426e4d9e600acfb8d7ca05f03b016d2dbe7
starryeye <see2818@naver.com>
2023-10-02 20:42:21 +0900

    [starryeye, 2023.10.02] - completableFuture practice project init
    내용
    - completableFuture practice project init

---
+++ completablefuture/practice/user/src/test/java/dev/practice/blocking/UserBlockingServiceTest.java
@@ -0,0 +1,71 @@
+package dev.practice.blocking;
+
+import dev.practice.blocking.repository.ArticleRepository;
+import dev.practice.blocking.repository.FollowRepository;
+import dev.practice.blocking.repository.ImageRepository;
+import dev.practice.blocking.repository.UserRepository;
+import dev.practice.common.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserBlockingServiceTest {
+    UserBlockingService userBlockingService;
+    UserRepository userRepository;
+    ArticleRepository articleRepository;
+    ImageRepository imageRepository;
+    FollowRepository followRepository;
+
+    @BeforeEach
+    void setUp() {
+
+        articleRepository = new ArticleRepository();
+        followRepository = new FollowRepository();
+        imageRepository = new ImageRepository();
+        userRepository = new UserRepository();
+
+        userBlockingService = new UserBlockingService(
+                articleRepository, followRepository, imageRepository, userRepository
+        );
+    }
+
+    @Test
+    void getUserEmptyIfInvalidUserIdIsGiven() {
+        // given
+        String userId = "invalid_user_id";
+
+        // when
+        Optional<User> user = userBlockingService.getUserById(userId);
+
+        // then
+        assertTrue(user.isEmpty());
+    }
+
+    @Test
+    void testGetUser() {
+        // given
+        String userId = "1234";
+
+        // when
+        Optional<User> optionalUser = userBlockingService.getUserById(userId);
+
+        // then
+        assertFalse(optionalUser.isEmpty());
+        var user = optionalUser.get();
+        assertEquals(user.getName(), "taewoo");
+        assertEquals(user.getAge(), 32);
+
+        assertFalse(user.getProfileImage().isEmpty());
+        var image = user.getProfileImage().get();
+        assertEquals(image.getId(), "image#1000");
+        assertEquals(image.getName(), "profileImage");
+        assertEquals(image.getUrl(), "https://dailyone.com/images/1000");
+
+        assertEquals(2, user.getArticleList().size());
+
+        assertEquals(1000, user.getFollowCount());
+    }
+}
