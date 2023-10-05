8dd1ed5028b4e653ddd733889eef1cf5c40e5440
starryeye <see2818@naver.com>
2023-10-06 02:27:00 +0900

    [starryeye, 2023.10.06] - Java NIO 를 이용한 non Blocking Server 개선
    내용
    - 내부 로직을 비동기 non-blocking 처리하여 성능 개선

--- server/javaio/src/main/java/dev/practice/nio/JavaNIONonBlockingMultiServer.java
+++ server/javaio/src/main/java/dev/practice/nio/JavaNIONonBlockingMultiServer.java
@@ -3,20 +3,26 @@ package dev.practice.nio;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
-public class JavaNIONonBlockingServer {
+public class JavaNIONonBlockingMultiServer {
 
     /**
      * Java NIO 기반의 서버
      * - non blocking 으로 동작
      */
+
+    private static AtomicInteger count = new AtomicInteger();
+    private static ExecutorService executorService = Executors.newFixedThreadPool(50);
 
     @SneakyThrows
     public static void main(String[] args) {
@@ -42,13 +48,25 @@ public class JavaNIONonBlockingServer {
                 }
 
                 /**
-                 * 아래 로직에서 10 ms 씩 잡아먹고 있어서 시간이 오래걸린다. 1000 번의 동시 요청 시 총 12 초가 걸림..
-                 *
                  * "main 입장에서 보면 아래 로직을 호출하는 것이 blocking 을 발생 시키고 있는 것이다."
+                 * -> 해결...
+                 * blocking 되는 로직을 main 스레드에서 비동기 non blocking 처리하였다.(다른 스레드로 처리)
+                 * 그러면..
+                 * main 스레드는 다음 요청을 받을 수 있는 준비 상태가 바로 될 것이다..
                  *
-                 * -> 해결 : JavaNIONonBlockingMultiServer
+                 * 스레드가 참 신기하네..
+                 * 분기된 스레드는 clientSocket 를 온전히 가지고.. close 까지 잘 처리할 것이고..
+                 * 메인 스레드는 기존 요청은 신경쓰지 않고 다음 요청을 처리할 수 있으니..
+                 *
+                 * 결과에 관심이 없다. 결과는 callee(다른 스레드) 가 처리한다. -> 비동기
+                 * 다른 스레드로 분기되어 처리되어 caller(main) 이 자기 할 일을 할 수 있다. -> non-blocking
+                 *
+                 * JavaIOMultiClient 로 수행결과.. 12초 걸리던것이 2초 정도로 크게 단축되었다.
                  */
-                handleRequest(clientSocket);
+                CompletableFuture.runAsync(() -> {
+                    handleRequest(clientSocket);
+                }, executorService);
+
             }
         }
     }
@@ -56,7 +74,7 @@ public class JavaNIONonBlockingServer {
     @SneakyThrows
     private static void handleRequest(SocketChannel clientSocket) {
         ByteBuffer requestByteBuffer = ByteBuffer.allocateDirect(1024);
-        while(clientSocket.read(requestByteBuffer) == 0) { // non-blocking 모드가 되면 read 가 반드시 값을 바로 준다는 보장이 없다..
+        while(clientSocket.read(requestByteBuffer) == 0) {
             log.info("server reading..");
         }
         requestByteBuffer.flip();
@@ -71,5 +89,8 @@ public class JavaNIONonBlockingServer {
         clientSocket.write(responseByteBuffer);
 
         clientSocket.close();
+
+        int c = count.incrementAndGet();
+        log.info("request count = {}", c);
     }
 }
