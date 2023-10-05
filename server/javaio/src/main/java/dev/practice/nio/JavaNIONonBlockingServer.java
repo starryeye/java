5469d79fddb3e02cbeb9492a94741ed833d9878a
starryeye <see2818@naver.com>
2023-10-05 21:32:06 +0900

    [starryeye, 2023.10.05] - Java NIO 를 이용한 non Blocking Server 문제점
    내용
    - 내부 로직이 blocking 을 발생시킨다.

--- server/javaio/src/main/java/dev/practice/nio/JavaNIONonBlockingServer.java
+++ server/javaio/src/main/java/dev/practice/nio/JavaNIONonBlockingServer.java
@@ -3,6 +3,7 @@ package dev.practice.nio;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
@@ -40,24 +41,33 @@ public class JavaNIONonBlockingServer {
                     continue; // null 이면 아래 로직 스킵
                 }
 
-                ByteBuffer requestByteBuffer = ByteBuffer.allocateDirect(1024);
-                while(clientSocket.read(requestByteBuffer) == 0) { // non-blocking 모드가 되면 read 가 반드시 값을 바로 준다는 보장이 없다..
-                    log.info("server reading..");
-                }
-                requestByteBuffer.flip();
-                String requestBody = StandardCharsets.UTF_8.decode(requestByteBuffer).toString();
-                log.info("request={}", requestBody);
-
-
-
-                Thread.sleep(10); // 외부 API 요청으로 인한 지연 가정
-
-
-                ByteBuffer responseByteBuffer = ByteBuffer.wrap("I am Server".getBytes());
-                clientSocket.write(responseByteBuffer);
-
-                clientSocket.close();
+                /**
+                 * 아래 로직에서 10 ms 씩 잡아먹고 있어서 시간이 오래걸린다. 1000 번의 동시 요청 시 총 12 초가 걸림..
+                 *
+                 * "main 입장에서 보면 아래 로직을 호출하는 것이 blocking 을 발생 시키고 있는 것이다."
+                 */
+                handleRequest(clientSocket);
             }
         }
+    }
+
+    @SneakyThrows
+    private static void handleRequest(SocketChannel clientSocket) {
+        ByteBuffer requestByteBuffer = ByteBuffer.allocateDirect(1024);
+        while(clientSocket.read(requestByteBuffer) == 0) { // non-blocking 모드가 되면 read 가 반드시 값을 바로 준다는 보장이 없다..
+            log.info("server reading..");
+        }
+        requestByteBuffer.flip();
+        String requestBody = StandardCharsets.UTF_8.decode(requestByteBuffer).toString();
+        log.info("request={}", requestBody);
+
+
+        Thread.sleep(10); // 외부 API 요청으로 인한 지연 가정
+
+
+        ByteBuffer responseByteBuffer = ByteBuffer.wrap("I am Server".getBytes());
+        clientSocket.write(responseByteBuffer);
+
+        clientSocket.close();
     }
 }
