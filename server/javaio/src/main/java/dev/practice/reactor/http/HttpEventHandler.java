0c2367aafe5ad97116979611da4e7f2eb3edffe4
starryeye <see2818@naver.com>
2023-10-09 17:37:34 +0900

    [starryeye, 2023.10.09] - Java NIO, selector, reactor pattern 를 이용한 non Blocking Server
    내용
    - HTTP Protocol 을 처리할 수 있음

---
+++ server/javaio/src/main/java/dev/practice/reactor/http/HttpEventHandler.java
@@ -0,0 +1,79 @@
+package dev.practice.reactor.http;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class HttpEventHandler implements EventHandler {
+
+    /**
+     * read 이벤트가 들어오면 처리한다.
+     *
+     * 즉, 기존에는 client 에서 tcp 요청(단순 문자열)이 server 들어왔는데..
+     * 이제 tcp 요청 값으로 http protocol 로 들어옴
+     * 여기서 http 처리를 해주면 되겠다!
+     */
+
+
+    private final Selector selector;
+    private final SocketChannel clientSocket;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(50); // 외부 API 지연 처리를 위한 별도의 스레드
+
+    private final MessageCodec messageCodec; // Http protocol 인코딩 디코딩을 위함
+
+    @SneakyThrows
+    public HttpEventHandler(Selector selector, SocketChannel socketChannel) {
+        this.selector = selector;
+        this.clientSocket = socketChannel;
+        this.messageCodec = new MessageCodec();
+
+        this.clientSocket.configureBlocking(false);
+
+        // read 이벤트를 등록하며.. reader(TcpEventHandler) 를 같이 넘긴다.
+        this.clientSocket.register(this.selector, SelectionKey.OP_READ).attach(this);
+    }
+
+    @Override
+    public void handle() {
+
+        // read 이벤트를 처리할 때 호출되는 메서드 이므로 read 반환값이 null 아님이 보장됨
+        String requestData = handleRequest(this.clientSocket);
+
+        sendResponse(this.clientSocket, requestData);
+    }
+
+    @SneakyThrows
+    private String handleRequest(SocketChannel clientSocket) {
+        ByteBuffer requestByteBuffer = ByteBuffer.allocateDirect(1024);
+        clientSocket.read(requestByteBuffer);
+
+        // read 한 ByteBuffer 를 디코딩하여 Http 쿼리 파라미터를 만들어 리턴
+        return messageCodec.decode(requestByteBuffer);
+    }
+
+    @SneakyThrows
+    private void sendResponse(SocketChannel clientSocket, String requestData) {
+
+        CompletableFuture.runAsync(() -> { // 외부 API 요청으로 인한 지연을 별도의 스레드로 처리한다.
+
+            try {
+                Thread.sleep(10); // 외부 API 요청으로 인한 지연 가정
+
+                ByteBuffer responseByteBuffer = messageCodec.encode(requestData); // 요청 데이터를 활용하여 응답값을 생성하고 인코딩
+                clientSocket.write(responseByteBuffer);
+
+                clientSocket.close();
+            } catch (Exception e) { /* do nothing */ }
+        }, executorService);
+    }
+}
---
+++ server/javaio/src/main/java/dev/practice/reactor/http/Reactor.java
@@ -0,0 +1,76 @@
+package dev.practice.reactor.http;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// [10]
+@Slf4j
+public class Reactor implements Runnable{ // Reactor 는 별도의 스레드에서 동작하므로 Runnable 을 구현
+
+    /**
+     * Reactor Pattern 을 적용한 Http 서버 구현
+     * - [9] 에서 모두 동일하며 TcpEventHandler 에서 HttpEventHandler 로 수정하여 http 를 다루도록 함
+     * - Http protocol 을 인코딩 디코딩하는 MessageCodec 이 추가됨
+     *
+     * - http/reactor.http 를 실행해보자. 요청 보낸 쿼리 파라미터를 활용한 응답을 받을 수 있다.
+     * - 혹은 웹브라우저에서 수행해보자..
+     */
+
+    private static ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final ServerSocketChannel serverSocket;
+    private final Selector selector; // reactor pattern 의 이벤트 multiplexing 을 위함
+    private final EventHandler acceptor;
+
+    @SneakyThrows
+    public Reactor(int port) {
+        this.selector = Selector.open();
+        this.serverSocket = ServerSocketChannel.open();
+        this.serverSocket.bind(new InetSocketAddress("localhost", port));
+        this.serverSocket.configureBlocking(false);
+
+        this.acceptor = new Acceptor(selector, serverSocket);
+        // accept 이벤트를 등록하며.. acceptor 를 같이 넘긴다.
+        this.serverSocket.register(selector, SelectionKey.OP_ACCEPT).attach(this.acceptor);
+    }
+
+    @Override
+    public void run() {
+        // 외부 main 에서 실행됨
+
+        log.info("start server");
+
+        executorService.submit(() -> {
+            while (true) {
+
+                selector.select();
+
+                Iterator<SelectionKey> selectedKeys = selector.selectedKeys().iterator();
+                while (selectedKeys.hasNext()) {
+                    SelectionKey key = selectedKeys.next();
+                    selectedKeys.remove();
+
+                    dispatch(key);
+                }
+            }
+        });
+    }
+
+    private void dispatch(SelectionKey key) {
+
+        // 이벤트 처리 모듈 및 메서드 호출
+
+        EventHandler eventHandler = (EventHandler) key.attachment(); // 이벤트 등록시 같이 넘겨준 오브젝트 받기
+
+        if (key.isAcceptable() || key.isReadable()) {
+            eventHandler.handle();
+        }
+    }
+}
