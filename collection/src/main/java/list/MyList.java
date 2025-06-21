f6da906db1497e8dc2c7c4446649558564a50b22
starryeye <see2818@naver.com>
2025-06-21 20:55:34 +0900

    [starryeye, 2025.06.21] - collection, ArrayList, LinkedList
    내용
    - collection, ArrayList, LinkedList

--- collection/src/main/java/list/MyList.java
+++ collection/src/main/java/list/MyList.java
@@ -2,6 +2,13 @@ package list;
 
 public interface MyList<E> {
 
+    /**
+     * List
+     * 순서(index)가 존재한다.
+     * element 중복 적재를 허용한다.
+     * 크기가 동적으로 변할 수 있다.
+     */
+
     void add(E e);
 
     void add(int index, E e);
