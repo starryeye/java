16cb0a76ef794673f56345d050b3b6d28e7de5f4
starryeye <see2818@naver.com>
2025-06-22 20:54:27 +0900

    [starryeye, 2025.06.22] - collection, ArrayList, LinkedList
    내용
    - collection, ArrayList, LinkedList

--- collection/src/main/java/list/MyList.java
+++ collection/src/main/java/list/MyList.java
@@ -4,7 +4,7 @@ public interface MyList<E> {
 
     /**
      * List
-     * 순서(index)가 존재한다.
+     * 순서가 존재한다. (ArrayList 는 내부 배열 index 가 곧 순서이며, LinkedList 에는 링크 연결 순서가 index 로 쓰인다.)
      * element 중복 적재를 허용한다.
      * 크기가 동적으로 변할 수 있다.
      */
