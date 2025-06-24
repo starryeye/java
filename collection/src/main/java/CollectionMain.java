7d0b9297a5e6c6b022b31a39defaa0f1cee39023
starryeye <see2818@naver.com>
2025-06-24 21:30:18 +0900

    [starryeye, 2025.06.24] - collection, HashSet
    내용
    - collection, HashSet

---
+++ collection/src/main/java/CollectionMain.java
@@ -0,0 +1,9 @@
+public class CollectionMain {
+
+    /**
+     * Java 의 Collection 은 제네릭 타입 기반이라서 primitive type 을 지원하지 않는다.
+     *      Java 의 제네릭은 primitive 타입(int, double, char, ...)을 직접 사용할 수 없음
+     *      또한, Hash 자료구조는 hashcode()를 오버라이딩하고 반환 값인 해시 코드를 사용해서 해시 인덱스를 만들어내기 때문에 primitive type 은 더욱 사용이 어려움
+     *      따라서, unboxing, boxing 을 이용해서 Wrapper type 을 사용하자
+     */
+}
