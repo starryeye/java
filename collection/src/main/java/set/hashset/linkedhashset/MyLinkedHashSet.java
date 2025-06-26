845b3783ca41522e71325b661de1ceddd3d55cdb
starryeye <see2818@naver.com>
2025-06-26 21:05:16 +0900

    [starryeye, 2025.06.26] - collection, Set
    내용
    - collection, Set

--- collection/src/main/java/set/hashset/linkedhashset/MyLinkedHashSet.java
+++ collection/src/main/java/set/hashset/linkedhashset/MyLinkedHashSet.java
@@ -6,7 +6,7 @@ public class MyLinkedHashSet<E> extends MyHashSet<E> {
 
     /**
      * HashSet 을 상속 받는 구조이다.
-     * Set 이지만, 순서를 보장한다는게 특징이다..
+     * Set 이지만, 순서를 보장한다는게 특징이다.. (입력된 순서)
      */
 
 
--- collection/src/main/java/set/treeset/MyTreeSet.java
+++ collection/src/main/java/set/treeset/MyTreeSet.java
@@ -5,8 +5,8 @@ import set.MySet;
 public class MyTreeSet<E> implements MySet<E> {
 
     /**
-     * binary search tree 를 개선한 red-black tree 를 내부적으로 사용한다.
-     * 따라서, 원소들이 정렬된 순서로 유지된다.
+     * binary search tree 를 개선한 red-black tree 를 내부적으로 사용한다. (지속적으로 트리의 균형을 유지하도록 개선)
+     * 따라서, 원소들이 정렬된 순서로 유지된다. (Comparator)
      * 주요 연산 시간 복잡도 : O(log n)
      *
      */
