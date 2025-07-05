e0109544e1e7632dade48ed0139b587f394f7484
starryeye <see2818@naver.com>
2025-07-06 03:29:47 +0900

    [starryeye, 2025.07.06] - Collection, Comparator Comparable
    내용
    -   Collection, Comparator Comparable

---
+++ collection/src/main/java/comparable/JavaArrayComparatorTest.java
@@ -0,0 +1,43 @@
+package comparable;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class JavaArrayComparatorTest {
+
+    public static void main(String[] args) {
+
+        Integer[] array = new Integer[]{3, 2, 1};
+        System.out.println("original = " + Arrays.toString(array)); // 3, 2, 1
+
+        // natural ordering (comparator 를 통하지 않고 정렬)
+        Arrays.sort(array);
+        System.out.println("Natural ordering sorted = " + Arrays.toString(array)); // 1, 2, 3
+
+
+        // 내림차순 정렬 Comparator<Integer> 을 통해 정렬
+        Arrays.sort(array, new DescComparator());
+        System.out.println("descending ordering sorted = " + Arrays.toString(array)); // 3, 2, 1
+
+
+        // 오름차순 정렬 Comparator<Integer> 을 통해 정렬
+        Arrays.sort(array, new AscComparator());
+        System.out.println("ascending ordering sorted = " + Arrays.toString(array)); // 1, 2, 3
+    }
+
+    private static class AscComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+
+            return (o1 < o2) ? -1 : ((o1 == o2) ? 0 : 1);
+        }
+    }
+
+    private static class DescComparator implements Comparator<Integer> {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+
+            return ((o1 < o2) ? -1 : ((o1 == o2) ? 0 : 1)) * -1;
+        }
+    }
+}
