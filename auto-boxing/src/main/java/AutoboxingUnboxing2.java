9b91ebc385a3a4da160ac43aa0bdf91059e083ae
starryeye <see2818@naver.com>
2025-07-11 00:41:36 +0900

    [starryeye, 2025.07.11] - Auto boxing, Auto unboxing
    내용
    -   Auto boxing, Auto unboxing

--- auto-boxing/src/main/java/AutoboxingUnboxing2.java
+++ auto-boxing/src/main/java/AutoboxingUnboxing2.java
@@ -39,10 +39,21 @@ public class AutoboxingUnboxing2 {
          * 코드상으로 보면 서로다른 인스턴스이고 reference type 간 동일성 비교이므로 false 가 나와야 할 것 같지만..
          * Integer caching 때문에 -128 ~ 127 범위의 정수 값은 동일한 인스턴스를 가리키게 되어 true 가 나온다.
          *
-         * 마치 String pool 과 비슷하게 되는데.. 그래도 값 비교시에는 항상 eqauls() 메서드를 사용하도록 하자.
+         * 참고.
+         * 1. 마치 String pool 과 비슷하게 되는데.. 그래도 값 비교시에는 항상 eqauls() 메서드를 사용하도록 하자.
+         * 2. 항상 타입을 주의 깊게 봐야한다..
+         *      Integer e = 127; Integer f = 127;
+         *      String g = "aaa"; String h = "aaa";
+         *          오른쪽만 보면 뭔가 stack 메모리에 값만 저장하는 것 같아서 == 비교 시 값비교만 할 것 같은데.. 주의해야함.
+         *          타입이 reference type 이라서 항상 객체 인스턴스가 heap 에 생성되고 참조 주소값이 저장된다.
+         *          타입이 reference type 이면, 127, "aaa" "이건 객체다" 라고 주문을 외워야한다.
          */
         Integer e = 127;
         Integer f = 127;
-        System.out.println("e == f -> " + (e == f));
+        System.out.println("e == f -> " + (e == f)); // 참조 주소 비교, Integer caching, true
+
+        String g = "abc";
+        String h = "abc";
+        System.out.println("g == h -> " + (g == h)); // 참조 주소 비교, String pool, true
     }
 }
