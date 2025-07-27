42e48ce84b29d6a0143677777c956b6d56c7b9ad
starryeye <see2818@naver.com>
2025-07-27 18:34:34 +0900

    [starryeye, 2025.07.27] - Thread
    내용
    - Thread

--- thread/src/main/java/concurrent/lock/sub2_condition/producer_consumer/BoundedBufferQueueV2.java
+++ thread/src/main/java/concurrent/lock/sub2_condition/producer_consumer/BoundedBufferQueueV2.java
@@ -18,17 +18,19 @@ public class BoundedBufferQueueV2 implements BoundedBufferQueue {
      *
      * 한정된 버퍼 문제를 해결하기 위해..
      * ReentrantLock, Condition::await(), signal() 를 사용하였다.
+     *      여기서 사용되는 락과 "대기 스레드 큐" 공간, "락 획득 대기 큐" 공간은..
+     *          Java 객체의 기본 모니터락과 "대기 스레드 큐" 공간, "락 획득 대기 큐" 공간이 아니다.
      *
      * 이전 V1 에서 Object::notify() 를 호출하면 대기 스레드 큐에 있는 스레드 중 하나를 깨우는데..
      * 다음과 같은 문제가 있다.
-     *      1. 버퍼가 가득차있고 대기 스레드 큐에 생산자 스레드들이 많은 경우..
+     *      1. 버퍼가 가득차있고 "대기 스레드 큐"에 생산자 스레드들이 많은 경우..
      *          소비자 스레드가 데이터를 소비하고 생산자 스레드들 중 하나가 깨워져서 생산을 정상적으로 했는데
-     *          생산 후 notify() 에 의해 생산자 스레드가 깨워지는 문제가 존재한다.(데이터는 이미 가득 차있는데 생산자 스레드가 깨워져서 아무것도 못하고 다시 대기큐에 진입)
-     *      2. 버퍼가 비워져있고 대기 스레드 큐에 소비자 스레드들이 많은 경우..
+     *          생산 후 notify() 에 의해 생산자 스레드가 깨워지는 문제가 존재한다.(데이터는 이미 가득 차있는데 생산자 스레드가 깨워져서 아무것도 못하고 다시 "대기 스레드 큐"에 진입)
+     *      2. 버퍼가 비워져있고 "대기 스레드 큐"에 소비자 스레드들이 많은 경우..
      *          생산자 스레드가 데이터를 생산하고 소비자자 스레드들 중 하나가 깨워져서 데이터 소비를 정상적으로 했는데
-     *          소비 후 notify() 에 의해 소비자 스레드가 깨워지는 문제가 존재한다.(데이터는 이미 없어졌는데 소비자 스레드가 깨워져서 아무것도 못하고 다시 대기큐에 진입)
+     *          소비 후 notify() 에 의해 소비자 스레드가 깨워지는 문제가 존재한다.(데이터는 이미 없어졌는데 소비자 스레드가 깨워져서 아무것도 못하고 다시 "대기 스레드 큐"에 진입)
      * 1, 2 번 문제를 위해
-     * 스레드 대기 큐를 2개로 분리해서 소비자 스레드는 생산자 스레드만 깨우고 생산자 스레드는 소비자 스레드만 깨울 수 있도록 한다.
+     * "대기 스레드 큐"를 2개로 분리해서 소비자 스레드는 생산자 스레드만 깨우고 생산자 스레드는 소비자 스레드만 깨울 수 있도록 한다.
      *
      * ReentrantLock..
      *      임계 영역에 접근하는 스레드를 1개로 하기 위함
@@ -36,12 +38,12 @@ public class BoundedBufferQueueV2 implements BoundedBufferQueue {
      * Condition::await()
      *      임계영역내에서 대기할 때 락 반납을 같이하기 위함
      *      호출한 스레드는 RUNNABLE 상태에서 WAITING 상태가 됨
-     *      호출한 스레드는 임계영역내의 대기 스레드 큐로 진입하고 락을 반납함
+     *      호출한 스레드는 임계영역내의 "대기 스레드 큐"로 진입하고 락을 반납함
      * Condition::signal()
-     *      임계영역내에서 대기하는 스레드(대기 스레드 큐의 스레드)들 중 하나를 깨운다.
-     *      깨워진 스레드는 WAITING 상태에서 BLOCKED 상태가 됨
+     *      임계영역내에서 대기하는 스레드("대기 스레드 큐"의 스레드)들 중 하나를 깨운다. (Queue 구조로 구현되어있어서 FIFO 로 깨운다.)
+     *      깨워진 스레드는 WAITING 상태로 "락 획득 대기 큐"로 진입
      *      깨워진 스레드는 깨운 스레드가 임계영역을 벗어나서 락을 반납하면..
-     *          락을 획득하고 BLOCKED 상태에서 RUNNABLE 상태가 된다.
+     *          락을 획득하고 WAITING 상태에서 RUNNABLE 상태가 된다.
      *
      */
 
@@ -50,8 +52,8 @@ public class BoundedBufferQueueV2 implements BoundedBufferQueue {
     private final int max;
 
     private final Lock lock = new ReentrantLock();
-    private final Condition producerCond = lock.newCondition(); // 생산자 스레드 대기 큐
-    private final Condition consumerCond = lock.newCondition(); // 소비자 스레드 대기 큐
+    private final Condition producerCond = lock.newCondition(); // 생산자 "대기 스레드 큐"
+    private final Condition consumerCond = lock.newCondition(); // 소비자 "대기 스레드 큐"
 
     public BoundedBufferQueueV2(int max) {
         this.max = max;
@@ -67,14 +69,14 @@ public class BoundedBufferQueueV2 implements BoundedBufferQueue {
             while (queue.size() == max) {
                 threadLog("[put] queue is full, producer condition await() is call");
                 try {
-                    producerCond.await(); // 현재 스레드, RUNNABLE -> WAITING, 락 반납, 대기 스레드 큐(생산자 전용) 진입
-                    threadLog("[put] producer awake"); // 현재 스레드, BLOCKED -> RUNNABLE, 락 획득
+                    producerCond.await(); // 현재 스레드, RUNNABLE -> WAITING, 락 반납, "대기 스레드 큐"(생산자 전용) 진입
+                    threadLog("[put] producer awake"); // 현재 스레드, WAITING -> RUNNABLE, 락 획득
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
             queue.offer(data);
-            consumerCond.signal(); // 소비자 전용 스레드 큐의 대기 스레드 중 하나, WAIT -> BLOCKED
+            consumerCond.signal(); // 소비자 전용 "대기 스레드 큐"의 대기 스레드 중 하나, WAITING 상태로 "락 획득 대기 큐"로 진입
             threadLog("[put] put data(" + data + ") in queue, consumer condition signal() is call");
 
 
@@ -92,14 +94,14 @@ public class BoundedBufferQueueV2 implements BoundedBufferQueue {
             while (queue.isEmpty()) {
                 threadLog("[take] queue is empty, consumer condition await() is call");
                 try {
-                    consumerCond.await(); // 현재 스레드, RUNNABLE -> WAITING, 락 반납, 대기 스레드 큐(소비자 전용) 진입
-                    threadLog("[take] consumer awake"); // 현재 스레드, BLOCKED -> RUNNABLE, 락 획득
+                    consumerCond.await(); // 현재 스레드, RUNNABLE -> WAITING, 락 반납, "대기 스레드 큐"(소비자 전용) 진입
+                    threadLog("[take] consumer awake"); // 현재 스레드, WAITING -> RUNNABLE, 락 획득
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             }
             String data = queue.poll();
-            producerCond.signal(); // 생산자 전용 스레드 큐의 대기 스레드 중 하나, WAIT -> BLOCKED
+            producerCond.signal(); // 생산자 전용 "대기 스레드 큐"의 대기 스레드 중 하나, WAITING 상태로 "락 획득 대기 큐"로 진입
             threadLog("[take] get data(" + data + ") from queue, producer condition signal() is call");
             return data;
 
--- thread/src/main/java/thread/state/sub7_producer_consumer/BoundedBufferQueueV1.java
+++ thread/src/main/java/thread/state/sub7_producer_consumer/BoundedBufferQueueV1.java
@@ -11,15 +11,18 @@ public class BoundedBufferQueueV1 implements BoundedBufferQueue {
 
     /**
      * 사전 지식.
-     *      모든 객체에는 모니터 락과 대기 스레드 큐가 존재한다.
+     *      모든 객체에는 모니터 락과 "대기 스레드 큐", "락 획득 대기 큐"가 존재한다.
      *      모니터 락
      *          synchronized 키워드를 사용할 경우 임계영역에 진입하는 스레드가 획득해야하는 락이다.
      *          1개의 스레드만 진입 가능, 임계영역을 벗어나면 획득했던 락을 반납한다.
-     *          락 획득을 대기하는 스레드는 BLOCKED 상태로 차단된다. (cpu 소모 X)
-     *      대기 스레드 큐
+     *          락 획득을 대기하는 스레드는 BLOCKED 상태로 차단된다. (cpu 소모 X, "락 획득 대기 큐" 에 진입)
+     *      "대기 스레드 큐"
      *          임계 영역 내에서 Object::wait() 를 호출한 스레드가 진입하여 대기하는 공간이다. (RUNNALBE -> WAITING, 락 반납)
-     *          임계 영역 내에서 Object::notify() 를 호출하면 대기 스레드 큐에 존재하는 스레드 중 하나가 깨워진다. (WAITING -> BLOCKED)
-     *              notify() 를 호출한 스레드가 임계영역을 벗어나서 락을 반납하면, 대기 스레드 큐에서 깨워진 스레드가 락을 획득한다. (BLOCKED -> RUNNABLE)
+     *          임계 영역 내에서 Object::notify() 를 호출하면 "대기 스레드 큐"에 존재하는 스레드 중 하나가 깨워진다. (WAITING -> BLOCKED, "락 획득 대기 큐"로 간다)
+     *              notify() 를 호출한 스레드가 임계영역을 벗어나서 락을 반납하면, "대기 스레드 큐"에서 깨워진 스레드가 락을 획득한다. (BLOCKED -> RUNNABLE)
+     *      "락 획득 대기 큐"
+     *          synchronized 키워드를 사용할 경우 모니터락을 획득하지 못하여 임계영역 밖에서 대기하는 스레드의 대기 공간이다. (RUNNABLE -> BLOCKED)
+     *          임계 영역 내의 스레드가 락을 반납하면 "락 획득 대기 큐"에 있던 스레드 중 하나가 락을 획득 하면서 임계 영역 내로 진입할 수 있다. (BLOCKED -> RUNNABLE)
      *
      * 한정된 버퍼 문제를 해결하기 위해..
      * synchronized, Object::wait(), notify() 를 사용하였다.
@@ -41,17 +44,17 @@ public class BoundedBufferQueueV1 implements BoundedBufferQueue {
      * Object::wait()
      *      임계영역내에서 대기할 때 락 반납을 같이하기 위함 (해결된 데드락 문제1, 2 를 위함)
      *      호출한 스레드는 RUNNABLE 상태에서 WAITING 상태가 됨
-     *      호출한 스레드는 임계영역내의 대기 스레드 큐로 진입하고 락을 반납함
+     *      호출한 스레드는 임계영역내의 "대기 스레드 큐"로 진입하고 락을 반납함
      * Object::notify()
-     *      임계영역내에서 대기하는 스레드(대기 스레드 큐의 스레드)들 중 하나를 깨운다.
-     *      깨워진 스레드는 WAITING 상태에서 BLOCKED 상태가 됨
+     *      임계영역내에서 대기하는 스레드("대기 스레드 큐"의 스레드)들 중 하나를 깨운다. (JVM 구현에 따라 스레드가 깨워지는 순서가 다름, 랜덤이라 생각해야함)
+     *      깨워진 스레드는 WAITING 상태에서 BLOCKED 상태가 됨 ("락 획득 대기 큐"로 진입)
      *      깨워진 스레드는 깨운 스레드가 임계영역을 벗어나서 락을 반납하면..
      *          락을 획득하고 BLOCKED 상태에서 RUNNABLE 상태가 된다.
      *
      * 참고.
      * Object::notifyAll()
-     *      임계영역내에서 대기하는 스레드(대기 스레드 큐의 스레드)들 모두를 깨운다.
-     *      깨워진 모든 스레드는 WAITING 상태에서 BLOCKED 상태가 됨
+     *      임계영역내에서 대기하는 스레드("대기 스레드 큐"의 스레드)들 모두를 깨운다.
+     *      깨워진 모든 스레드는 WAITING 상태에서 BLOCKED 상태가 됨 ("락 획득 대기 큐"로 진입)
      *      깨워진 스레드는 깨운 스레드가 임계영역을 벗어나서 락을 반납하면..
      *          락을 획득하고 BLOCKED 상태에서 RUNNABLE 상태가 된다.
      *          이후 해당 스레드가 락을 반납하면 다음 스레드가 동일하게 락을 획득할 수 있다.
@@ -74,14 +77,14 @@ public class BoundedBufferQueueV1 implements BoundedBufferQueue {
         while (queue.size() == max) {
             threadLog("[put] queue is full, wait() is call");
             try {
-                wait(); // 현재 스레드, RUNNABLE -> WAITING, 락 반납, 대기 스레드 큐 진입
+                wait(); // 현재 스레드, RUNNABLE -> WAITING, 락 반납, "대기 스레드 큐" 진입
                 threadLog("[put] producer awake"); // 현재 스레드, BLOCKED -> RUNNABLE, 락 획득
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
         queue.offer(data);
-        notify(); // 대기 스레드 중 하나, WAIT -> BLOCKED
+        notify(); // "대기 스레드 큐"의 대기 스레드 중 하나, WAITING -> BLOCKED, "락 획득 대기 큐"로 진입
         threadLog("[put] put data(" + data + ") in queue, notify() is call");
     }
 
@@ -97,7 +100,7 @@ public class BoundedBufferQueueV1 implements BoundedBufferQueue {
             }
         }
         String data = queue.poll();
-        notify(); // 대기 스레드 중 하나, WAIT -> BLOCKED
+        notify(); // "대기 스레드 큐"의 대기 스레드 중 하나, WAITING -> BLOCKED, "락 획득 대기 큐"로 진입
         threadLog("[take] get data(" + data + ") from queue, notify() is call");
         return data;
     }
