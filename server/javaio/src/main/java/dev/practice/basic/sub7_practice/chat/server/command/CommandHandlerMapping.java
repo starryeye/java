18ee05f390334caed9e8864dfbdc76a2f025c6a0
starryeye <see2818@naver.com>
2025-09-11 21:18:02 +0900

    [starryeye, 2025.09.11] - network io
    내용
    - network io

--- server/javaio/src/main/java/dev/practice/basic/sub7_practice/chat/server/command/CommandHandlerMapping.java
+++ server/javaio/src/main/java/dev/practice/basic/sub7_practice/chat/server/command/CommandHandlerMapping.java
@@ -14,6 +14,8 @@ public class CommandHandlerMapping {
     }
 
     public void execute(String request, Session session) throws IOException {
+
+        // todo, execute 에서 Session 파라미터 제거하고 SessionManager 로 control 해보기.. 특정 Session 을 원하면 session 의 username 으로.. session 찾기
         if (request.startsWith("/exit")) {
             throw new IOException("exit");
         }
