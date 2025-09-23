970fcf50de09ec1a2d659525e14d6a7ea19f97ff
starryeye <see2818@naver.com>
2025-09-23 22:16:53 +0900

    [starryeye, 2025.09.23] - java reflection
    내용
    - java reflection

---
+++ reflection-basic/src/main/java/reflection/sub7_annotation_metadata/AnnotationMetadata.java
@@ -0,0 +1,13 @@
+package reflection.sub7_annotation_metadata;
+
+public interface AnnotationMetadata {
+
+    /**
+     * annotation 에 대해 따로 설명이 필요하므로
+     * annotation project 에서 따로 다룬다.
+     *      annotation.sub2_meta_annotation.inherited.CheckInherited
+     *          annotation 메타데이터 다루기
+     *      annotation.sub3_practice
+     *          reflection + annotation 활용
+     */
+}
