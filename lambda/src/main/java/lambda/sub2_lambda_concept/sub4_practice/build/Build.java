be311ff6d1260a95975a6b5a2087a1ab6cef4dc8
starryeye <see2818@naver.com>
2025-10-02 19:43:09 +0900

    [starryeye, 2025.10.02] - java lambda
    내용
    - java lambda

--- lambda/src/main/java/lambda/sub2_lambda_concept/sub4_practice/build/Build.java
+++ lambda/src/main/java/lambda/sub2_lambda_concept/sub4_practice/build/Build.java
@@ -2,5 +2,16 @@ package lambda.sub2_lambda_concept.sub4_practice.build;
 
 public class Build {
 
-    //todo
+    // 고차 함수, 함수를 반환하는 함수
+    private static MyStringFunction buildGreeter(String greeting) {
+        return name -> greeting + ", " + name;
+    }
+
+    public static void main(String[] args) {
+        MyStringFunction greeter1 = buildGreeter("Hello");
+        MyStringFunction greeter2 = buildGreeter("Hi");
+
+        System.out.println(greeter1.apply("NickName1"));        // MyStringFunction 실행 -> Hello, NickName1
+        System.out.println(greeter2.apply("NickName2"));        // MyStringFunction 실행 -> Hi, NickName2
+    }
 }
--- lambda/src/main/java/lambda/sub2_lambda_concept/sub4_practice/compose/Compose.java
+++ lambda/src/main/java/lambda/sub2_lambda_concept/sub4_practice/compose/Compose.java
@@ -2,5 +2,25 @@ package lambda.sub2_lambda_concept.sub4_practice.compose;
 
 public class Compose {
 
-    // todo
+    // 두 개의 함수형 인터페이스를 순서대로 합성해주는 고차함수
+
+    // 고차 함수, 함수를 매개변수로 받고 함수를 반환하는 함수
+    private static MyStringFunction compose(MyStringFunction f1, MyStringFunction f2) {
+
+        return str -> {
+            String applied1 = f1.apply(str);
+            String applied2 = f2.apply(applied1);
+            return applied2;
+        };
+    }
+
+    public static void main(String[] args) {
+        MyStringFunction f1 = s -> s.toUpperCase();
+        MyStringFunction f2 = s -> "***" + s + "***";
+
+        MyStringFunction composed = compose(f1, f2);
+        String result = composed.apply("Hello World");// 합성된 MyStringFunction 실행
+
+        System.out.println("result = " + result);
+    }
 }
