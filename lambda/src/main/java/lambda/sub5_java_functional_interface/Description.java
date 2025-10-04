bf2b85c2afed9043d0a79c4e98ed25d0bcde8a52
starryeye <see2818@naver.com>
2025-10-04 16:21:17 +0900

    [starryeye, 2025.10.04] - java lambda
    내용
    - java lambda

--- lambda/src/main/java/lambda/sub5_java_functional_interface/Description.java
+++ lambda/src/main/java/lambda/sub5_java_functional_interface/Description.java
@@ -52,7 +52,7 @@ public interface Description {
      *              public interface BinaryOperator<T> extends BiFunction<T,T,T> {}
      *              BiFunction 을 상속받아서 두개의 매개변수 타입과 결과 타입이 동일한 의도에 특화
      *
-     * 기본형 특화
+     * 기본형(primitive) 특화
      *      java.util.function
      *          IntFunction
      *              @FunctionalInterface
@@ -62,5 +62,15 @@ public interface Description {
      *              auto-boxing/unboxing 의 성능 비용을 줄이고 제네릭이 primitive type 을 다룰 수 없음을 극복하기 위한 특화
      *              모든 primitive type 을 prefix 로 하여 기본형 함수형 인터페이스를 붙이는 네이밍으로 존재한다.
      *                  IntFunction, IntConsumer, ..., DoubleSupplier, LongXXXXX, ....
+     *
+     * 확장?
+     *      Callable
+     *          @FunctionalInterface
+     *          public interface Callable<V> {
+     *              V call() throws Exception;
+     *          }
+     *          Supplier 와 비슷한데 예외를 던질 수 있도록 만들어짐 (Checked exception 외부 던지기용)
+     *              Callable 이 만들어진 이유 자체는, ExecutorService 이용시 Runnable 의 반환 값이 없는 것을 개선하기 위해 Future 와 함께 Callable 을 사용하기위해 만들어짐
+     *          하위 호환성을 위해 java.util.concurrent 패키지에 존재
      */
 }
