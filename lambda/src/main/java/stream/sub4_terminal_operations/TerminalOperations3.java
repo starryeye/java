250be1460c89c84aa6bd78178f710ad347fc7156
starryeye <see2818@naver.com>
2025-10-20 21:43:22 +0900

    [starryeye, 2025.10.20] - Java stream
    내용
    - Java stream

---
+++ lambda/src/main/java/stream/sub4_terminal_operations/TerminalOperations3.java
@@ -0,0 +1,14 @@
+package stream.sub4_terminal_operations;
+
+public class TerminalOperations3 {
+
+    /**
+     * 최종 연산자에 대해 알아본다.
+     *
+     * todo
+     *
+     * 1. collect
+     * 2. toList
+     * 3. toArray
+     */
+}
---
+++ lambda/src/main/java/stream/sub5_primitive_stream/PrimitiveStream1.java
@@ -0,0 +1,10 @@
+package stream.sub5_primitive_stream;
+
+public class PrimitiveStream1 {
+
+    /**
+     * 기본형 특화 스트림에 대해 알아본다.
+     *
+     * todo
+     */
+}
